8af1106a209e7f7da54655d1a2194942bcb42551
kennie <kennie747@gmail.com>
2022-10-29 09:09:47 +0100

    completed projec

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'o8xwnskoua';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-6llj17dtgf2qoi76.us.auth0.com',
+  clientId: 'fcsnrkIfgo8r8P2OV4goJ9POgWgRHTpA',
+  callbackUrl: 'http://localhost:3000/callback'
+};
